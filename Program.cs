c30ba05c799eb9cc7218dc0be2f0661b68c91767
Shubhamrazz <sr99845@gmail.com>
2022-05-07 14:00:55 +0530

    [Shubham]GenericDemo class

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericDemo_Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            //Program.toPrint(intArray);
+            //Program.toPrint(doubleArray);
+            //Program.toPrint(charArray);
+
+            Sample.toPrint<int>(intArray);
+            Sample.toPrint<double>(doubleArray);
+            Sample.toPrint<char>(charArray);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
